355b139b182a58d620613718faa1971bb945beaa
adriana634 <50183872+adriana634@users.noreply.github.com>
2022-10-02 19:45:33 +0200

    Initial Implementation

---
+++ FloatExtensions.cs
@@ -0,0 +1,10 @@
+﻿namespace DinoColors
+{
+    public static class FloatExtensions
+    {
+        public static bool IsEqualTo(this float a, float b)
+        {
+            return Math.Abs(a - b) < double.Epsilon;
+        }
+    }
+}
